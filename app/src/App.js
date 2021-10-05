6d59fa7592c6e0983489d6b51211b94d6227839b
AgustinaLuz <69695943+AgustinaLuz@users.noreply.github.com>
2021-10-05 19:22:13 -0300

    Revert "cute"

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,30 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Axios from "axios";
-import Entries from "./components/entries";
-import Amount from "./components/amount";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await Axios.get(
+      "http://localhost:3001/api/operations"
+    );
+    const products = data;
+    setProducts(products);
+    console.log(products);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <div>
-      <Entries />
-      <Amount />
+      {products.map((product) => (
+        <p key={product._id}>{product.concept} - {product.amount} - {product.date} - {product.category} - {product.type}</p>   
+      ))}
     </div>
   );
 }
+
 export default App;
